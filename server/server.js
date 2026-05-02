087eebec8c92f0db4d395964ed33f8e61e561ad1
kabirdev-byte <kabirjamshaid4@gmail.com>
2026-05-02 06:56:28 -0400

    fixed CORS URL - removed trailing slash

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(cors(
   {
   origin: [
     'http://localhost:5173',
-    'https://job-portal-new-xn38.vercel.app/'  // Apna actual frontend URL yahan
+    'https://job-portal-new-xn38.vercel.app'  // Apna actual frontend URL yahan
   ],
   credentials: true
 }
