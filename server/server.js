9eb9b4873f948babfb3e6d40f8ea24da25af1b5b
kabirdev-byte <kabirjamshaid4@gmail.com>
2026-05-30 11:46:57 -0400

    updated CORS for both auto-generated and short URL

--- server/server.js
+++ server/server.js
@@ -1,6 +1,3 @@
-
-
-
 import "./config/instrument.js";
 import express from "express";
 import cors from "cors";
@@ -8,12 +5,11 @@ import "dotenv/config";
 import connectDB from "./config/db.js";
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhooks.js";
-import companyRoutes from './routes/companyRoutes.js'
+import companyRoutes from "./routes/companyRoutes.js";
 import connectCloudinary from "./config/cloudinary.js";
-import jobRoutes from './routes/jobRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import {clerkMiddleware} from '@clerk/express'
-
+import jobRoutes from "./routes/jobRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 // Initialize Express
 const app = express();
@@ -22,24 +18,22 @@ const app = express();
 await connectDB();
 await connectCloudinary();
 
-
 // 🔹 Sentry request handler MUST be first
 app.use(Sentry.Handlers.requestHandler());
 
-
 // middlewares
-app.use(cors(
-  {
-  origin: [
-    'http://localhost:5173',
-    'https://kabir-job-portal.vercel.app'  // Apna actual frontend URL yahan
-  ],
-  credentials: true
-}
-));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://kabir-job-portal-afy7ue91s-kabirjamshaid4-9736s-projects.vercel.app", // ✅ AUTO-GENERATED URL
+      "https://kabir-job-portal.vercel.app", // Apna actual frontend URL yahan
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(clerkMiddleware());
-
 
 // Routes
 app.get("/", (req, res) => res.send("API working"));
@@ -48,9 +42,9 @@ app.get("/debug-sentry", (req, res) => {
   throw new Error("My first Sentry error!");
 });
 app.post("/webhooks", clerkWebhooks);
-app.use('/api/company', companyRoutes );
-app.use('/api/jobs', jobRoutes)
-app.use('/api/users', userRoutes)
+app.use("/api/company", companyRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/users", userRoutes);
 
 // 🔹 Sentry error handler MUST be after routes
 app.use(Sentry.Handlers.errorHandler());
@@ -59,4 +53,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
